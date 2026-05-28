1a2ca68587f1d59d67576fbcd19ecd7d2881f0cc
jayverner <jaylenverner@gmail.com>
2026-05-28 13:48:28 -0500

    Add files via upload

---
+++ array.js
@@ -0,0 +1,24 @@
+/*Create two separate arrays using array literals for the following:
+● An array of 5 grocery store items.
+● An array of 3 numbers representing prices.
+
+Task 2: Access and Log the Values
+Use console.log() to log the arrays.
+1. Log the entire array of grocery items.
+2. Log the entire array of prices.
+3. Log three individual items of your choice.
+4. Log two prices of your choice.
+*/
+
+let groceryList = ["milk", "eggs", "cheese", "fruit", "meat"];
+let price = [4.99, 7.99, 2.99, 1.99, 32.99];
+
+console.log(groceryList[0]);
+console.log(groceryList[1]);
+console.log(groceryList[2]);
+
+console.log(price[0]);
+console.log(price[1]);
+
+
+
